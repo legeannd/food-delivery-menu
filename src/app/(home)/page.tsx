a4fb6e2d2648f3ae6bc67fac8c2efec0336f61dc
Gean Lucas <gean.lfpsa@gmail.com>
2025-05-12 20:27:43 -0300

    fix: change home banner image url

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import Loading from "./loading";
 export default function Home() {
   return (
     <div className="flex flex-col grow">
-      <Banner src={"/images/banner.png"} alt={"Home banner"} />
+      <Banner src={"/assets/images/banner.png"} alt={"Home banner"} />
       <Suspense fallback={<Loading />}>
         <RestaurantList />
       </Suspense>
